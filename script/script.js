70c5ce79f35fabc09ec892574aa2c4a8bc5a00b2
robynainsley21 <robynainsley2i@gmail.com>
2024-05-17 10:23:00 +0200

    Edit functionality

--- script/script.js
+++ script/script.js
@@ -1,12 +1,8 @@
 //the screen display
 let dataEntry = document.querySelector("[data-entry]");
 
-//number value
-// let number = document.querySelector("[number]");
-
-// operator variable
+// operator variables
 const dataOp = document.querySelectorAll("[data-op]");
-
 const dataCalc = document.querySelector("[data-calc]");
 const clear = document.querySelector("[clear]");
 
@@ -33,7 +29,7 @@ const multiply = (num1, num2) => {
 };
 
 const divide = (num1, num2) => {
-  return num1 / num2;
+  return (num1 / num2).toFixed(2);
 };
 
 const modulus = (num1, num2) => {
@@ -41,14 +37,15 @@ const modulus = (num1, num2) => {
 };
 
 const calculate = () => {
-  console.log(dataOp.value);
-  let num1 = parseInt(calcArray[0]);
-  let num2 = parseInt(calcArray[2]);
+  let num1 = parseFloat(calcArray[0]);
+  let num2 = parseFloat(calcArray[2]);
 
+  //checking if length of array is 3
   if (calcArray.length !== 3) {
     display("Error: Incomplete expression");
   }
 
+  //checks for operator existence
   switch (calcArray[1]) {
     case "+":
       result = add(num1, num2);
@@ -74,22 +71,25 @@ const calculate = () => {
       display("Error: Invalid operation");
       break;
   }
+
+  calcArray = []
 };
 
 //because they are buttons, one needs a loop
-dataOp.forEach(operator => {
+dataOp.forEach((operator) => {
   operator.addEventListener("click", () => {
     calcArray.push(dataEntry.value);
     calcArray.push(operator.textContent.trim());
-    console.log(calcArray);
   });
 });
 
+//equal sign function
 dataCalc.addEventListener("click", () => {
   calcArray.push(dataEntry.value);
   calculate();
 });
 
+//clears array after 'del' or 'AC' button is clicked
 clear.addEventListener("click", () => {
   dataEntry.value = "";
   calcArray = [];
